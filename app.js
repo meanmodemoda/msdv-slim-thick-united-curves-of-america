424028102552c4eb1d799ace235276e7553b1084
EC2 Default User <ec2-user@ip-172-31-17-171.ec2.internal>
2021-11-29 23:41:33 +0000

    svg

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 /*global d3 */
+//npm d3-simple-slider
+//npm i @thomasloven/round-slider
 
 async function draw() {
 
-//*************************1. Draw Dimensions
+//*************************1. Draw Dimensions 
 
-   const width = 400;
-    
-    let dimensions = {
-        width: width,
+//------------------------ Area Chart Dimensions
+
+    const dimensionsArea = {
+        width: 400,
         height: 800,
         margin: {
             top: 20,
@@ -17,13 +19,35 @@ async function draw() {
         }
     }
 
-    dimensions.boundedWidth = dimensions.width
-        - dimensions.margin.left
-        - dimensions.margin.right
+    dimensionsArea.boundedWidth = dimensionsArea.width
+        - dimensionsArea.margin.left
+        - dimensionsArea.margin.right
 
-    dimensions.boundedHeight = dimensions.height
-        - dimensions.margin.top
-        - dimensions.margin.bottom
+    dimensionsArea.boundedHeight = dimensionsArea.height
+        - dimensionsArea.margin.top
+        - dimensionsArea.margin.bottom
+        
+//-----------------------Line Chart Dimensions        
+        
+   const dimensionsLine = {
+        width: 400,
+        height: 800,
+        margin: {
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 50
+        }
+    }
+
+    dimensionsLine.boundedWidth = dimensionsLine.width
+        - dimensionsLine.margin.left
+        - dimensionsLine.margin.right
+
+    dimensionsLine.boundedHeight = dimensionsLine.height
+        - dimensionsLine.margin.top
+        - dimensionsLine.margin.bottom
+        
     
 // const posCheck = document.querySelector("#section-intro-chart").addEventListener('mouseenter', (e) => {
 //         console.log("mouse page X: ", e.pageX);
@@ -31,13 +55,27 @@ async function draw() {
 //     }, );
         
 //*************************2. Draw Wrapper and Bounds
-   const wrapper = d3.select("#section-intro-chart") 
+
+//----------------------------Area Chart Wrapper and Bounds
+   const wrapperArea = d3.select("#section-intro-chart") 
         .append("svg")
-        .attr("width", dimensions.width)
-        .attr("height", dimensions.height)
+        .attr("width", dimensionsArea.width)
+        .attr("height", dimensionsArea.height)
         
-   const bounds = wrapper.append("g")
-        .style("translate",`transform(${dimensions.margin.left}px, ${dimensions.margin.top}px)`)
+   const boundsArea = wrapperArea.append("g")
+        .style("translate",`transform(${dimensionsArea.margin.left}px, ${dimensionsArea.margin.top}px)`)
+
+//----------------------------Line Chart Wrapper and Bounds
+
+   const wrapperLine = d3.select("#svg-group") 
+        .append("svg")
+        .attr("width", dimensionsLine.width)
+        .attr("height", dimensionsLine.height)
+        
+   const boundsLine = wrapperLine.append("g")
+        .style("translate",`transform(${dimensionsLine.margin.left}px, ${dimensionsLine.margin.top}px)`)
+
+
   // const tooltip = d3.select("#tooltip")
   
 //**************************3. Load Data and Create Accessors
@@ -45,9 +83,17 @@ async function draw() {
   const msm = await d3.csv("./body_measurement.csv",(d) => {
       d3.autoType(d) //time will converted to time format
         return d})
-        
 // console.table(msm[0]) //this is good for csv
 // console.log(msm[0])      
+
+ const surgery = await d3.csv("./plastic_surgery_growth.csv",(d) => {
+     d3.autoType(d)
+      return d})
+// console.table(surgery[0]) 
+// console.log(surgery[0])  
+
+const sumSry = d3.group(surgery, d => d.surgery);    
+// console.log(sumSry)
 
     //bodyType
 const typeAccessor = d => d.type;
@@ -61,6 +107,14 @@ const periodAccessor = d => d.period;
 const periodNumAccessor = d =>d.periodNum;
 // console.log(periodAccessor(msm[0]))
 
+//surgery year
+const yearParser = d3.timeParse("%Y");
+const yearAccessor = d =>yearParser(d.year);
+// console.log(yearAccessor(surgery[0]))
+//surgery rate
+const rateAccessor = d => d.rate;
+// console.log(rateAccessor(surgery[0]))
+
 //set up Arrays manually so I can control the order
 const bodyParts = ["bust","abdomen","waist","hip","butt"]
 const bodyTypes = ["Hour Glass","Heroine Chic","Pilates Body","Slim Thick"]
@@ -70,19 +124,27 @@ const periods = ["since Playboy","1990s","2000s","2010s - now"]
 
 const xScale = d3.scaleLinear()
     .domain([22,40])
-    .range([dimensions.margin.left,dimensions.margin.left+250])
+    .range([dimensionsArea.margin.left,dimensionsArea.margin.left+250])
     
 const yScale = d3.scaleOrdinal()
     .domain(bodyParts)
-    .range([dimensions.boundedHeight/2-250,dimensions.boundedHeight/2-150,dimensions.boundedHeight/2-50,dimensions.boundedHeight/2+100,dimensions.boundedHeight/2+200])
+    .range([dimensionsArea.boundedHeight/2-250,dimensionsArea.boundedHeight/2-150,dimensionsArea.boundedHeight/2-50,dimensionsArea.boundedHeight/2+100,dimensionsArea.boundedHeight/2+200])
     
 const colorScale = d3.scaleOrdinal()
     .domain(bodyTypes)
     .range(['#999999','#666666','#333333','#000000'])
     
+const timeScale = d3.scaleTime()
+    .domain(d3.extent(surgery,yearAccessor))
+    .range([dimensionsLine.boundedHeight,dimensionsLine.margin.top])
+    .nice()
+    
+const pctScale = d3.scaleLinear()
+    .domain(d3.extent(surgery))
+    
 // const sliderScale = d3.scaleOrdinal()
 //     .domain(periods)
-//     .range([dimensions.boundedWidth/2+100,dimensions.boundedWidth/2+200,dimensions.boundedWidth/2+300,dimensions.boundedWidth/2+400])
+//     .range([dimensionsArea.boundedWidth/2+100,dimensionsArea.boundedWidth/2+200,dimensionsArea.boundedWidth/2+300,dimensionsArea.boundedWidth/2+400])
     
 //****************************5. Draw Peripherals
 
@@ -101,27 +163,23 @@ const yAxisGenerator = d3.axisLeft()
     .tickSize(5)
 
 //----------------------Draw Axes
-const xAxisBottom = bounds.append("g")
+const xAxisBottom = boundsArea.append("g")
     .call(xAxisBottomGenerator)
-    .style("transform",`translateY(${dimensions.boundedHeight/2+200}px)`)
+    .style("transform",`translateY(${dimensionsArea.boundedHeight/2+200}px)`)
 
 // const sliderAxis = bounds.append("g")
 //     .call(sliderAxisGenerator)
-//     .style("transform",`translateY(${dimensions.boundedHeight/2+370}px)`)
+//     .style("transform",`translateY(${dimensionsArea.boundedHeight/2+370}px)`)
     
-const xAxisTop = bounds.append("g")
+const xAxisTop = boundsArea.append("g")
     .call(xAxisTopGenerator)
-    .style("transform",`translateY(${dimensions.boundedHeight/2-250}px)`)
+    .style("transform",`translateY(${dimensionsArea.boundedHeight/2-250}px)`)
     
-const yAxis = bounds.append("g")
+const yAxis = boundsArea.append("g")
     .call(yAxisGenerator)
-    .style("transform",`translateX(${dimensions.margin.left}px)`)
+    .style("transform",`translateX(${dimensionsArea.margin.left}px)`)
     
-//----------------------Draw Test Dots  
-const testDot = bounds.append("circle")
-    .attr("cx",dimensions.boundedWidth/2)
-    .attr("cy",dimensions.boundedHeight/2)
-    .attr("r", 5)
+
 
 //---------------------------Draw Ordinal Scale Slider    
 //using d3=simple-slider package, note syntax slightly different from d3.js
@@ -141,12 +199,12 @@ const sliderGenerator = d3.sliderHorizontal()
         .size(200)())
     
 
-const slider = bounds.append('g')
+const slider = boundsArea.append('g')
     .classed("slider",true)
     // .attr('width', 400)
     // .attr('height', 100)
     //note this is different from CSS, d3 standard `translate` syntax, `px` is omitted
-    .attr('transform', `translate(${dimensions.margin.left},${dimensions.boundedHeight/2+250})`)
+    .attr('transform', `translate(${dimensionsArea.margin.left},${dimensionsArea.boundedHeight/2+250})`)
     .call(sliderGenerator);
     
 //***************************6. Draw Chart Generator
@@ -175,7 +233,7 @@ function drawAreaChart(periodNum) {
     }   
     
  //--------------------Draw areaChart
-    bounds.selectAll(".path")
+    boundsArea.selectAll(".path")
     .data(sumMsm)
     .join("path")
     .attr("d", d=>areaGenerator(d[1]))
@@ -184,7 +242,7 @@ function drawAreaChart(periodNum) {
     // .attr("stroke", d => colorScale(d[0]))
     // .attr("stroke-width", 2)
     .attr("opacity",gradientChange(periodNum))
-    console.log(periodNum);
+    // console.log(periodNum);
 }
 
 //-----------------------------9. Draw Interaction
@@ -207,5 +265,41 @@ else   if(value==4){document.querySelector("#section-intro-description").textCon
 });
     
 drawAreaChart(1);
+
+
+//--------------------------------10 line tracing
+
+//----------------------Draw Test Dots  
+
+
+const drawing = d3.select("g#left-sil-line");
+const line = drawing.select("path");
+const sliderTest = d3.select("input#curve-slider");
+
+const testDot = drawing.append("circle")
+    .attr("cx",500)
+    .attr("cy",2000)
+    .attr("r", 40)
+    .attr("fill","blue")
+
+const getPos = () => {
+    let lineLength = line.node().getTotalLength();
+    let pct = Number(sliderTest.node().value);
+    let pos = line.node().getPointAtLength(pct * lineLength);
+    console.log(pos)
+    return pos;
+}
+
+const circle = drawing.append("circle")
+    .attr("cx",getPos().x)
+    .attr("cy",getPos().y)
+    .attr("r",20)
+    .attr("fill","blue")
+    
+sliderTest.on("input", () => {
+  // update coordinates of circle
+  // when input slider is updated
+  circle.attr("cx", getPos().x).attr("cy", getPos().y);
+})
    
 } draw()
