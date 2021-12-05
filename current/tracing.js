2550f682a392fb9500aab8a18a42812d2511b554
EC2 Default User <ec2-user@ip-172-31-17-171.ec2.internal>
2021-12-05 16:14:39 +0000

    update

--- current/tracing.js
+++ current/tracing.js
@@ -6,8 +6,9 @@ const lengthRight = svgRight.getTotalLength();
 
 // start position of the drawing - normal display pre-animation
 svgLeft.style.strokeDasharray = lengthLeft;
-
+svgRight.style.strokeDasharray = lengthRight;
 // hides the svg before the scrolling starts
+svgLeft.style.strokeDashoffset = lengthLeft;
 svgRight.style.strokeDashoffset = lengthRight;
 
 // offset the svg dash by the same amount as the percentage scrolled
@@ -17,14 +18,15 @@ window.addEventListener("scroll", function () {
   const scrollH = document.documentElement.scrollHeight
   const pixels =  window.pageYOffset-3000;
   const bodyT= document.body.scrollTop
-  const scrollpercent = 2000 / (scrollH-scrollT);
-  console.log("bodyT",bodyT)
-console.log("scrollTop",scrollT)
-console.log("scrollHeight",scrollH)
-console.log("pct",scrollpercent)
-  const drawLeft = lengthLeft * scrollpercent;
-  const drawRight = lengthRight * scrollpercent;
-console.log("draw",drawLeft)
+  const scrollpercentLeft = 2000 / (scrollH-scrollT);
+  const scrollpercentRight = 2050 / (scrollH-scrollT);
+//   console.log("bodyT",bodyT)
+// console.log("scrollTop",scrollT)
+// console.log("scrollHeight",scrollH)
+// console.log("pct",scrollpercent)
+  const drawLeft = lengthLeft * scrollpercentLeft;
+  const drawRight = lengthRight * scrollpercentRight;
+// console.log("draw",drawLeft)
   // Reverse the drawing (when scrolling upwards)
   svgLeft.style.strokeDashoffset = lengthLeft - drawLeft;
  svgRight.style.strokeDashoffset = lengthRight - drawRight;
