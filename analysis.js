07a5c162c47654a836109ebca7c605a24db8013c
Yang Zhao <yangzhao@Yangs-MacBook-Pro.local>
2021-12-10 21:42:26 -0500

    update

--- analysis.js
+++ analysis.js
@@ -6,7 +6,7 @@ async function drawAnalysis() {
     //------------------------ Area Chart Dimensions     
 
     const dimensionsLine = {
-        width: 600,
+        width: 800,
         height: 800,
         margin: {
             top: 20,
@@ -49,6 +49,9 @@ async function drawAnalysis() {
     const lineChartGroup = boundsLine.append("g")
         .classed("lineChart", true)
 
+    const compChartGroup = boundsLine.append("g")
+        .classed("lineCompChart", true)
+
     //**************************3. Load Data and Create Accessors
 
     //----------------------Load Data
@@ -61,16 +64,23 @@ async function drawAnalysis() {
         d3.autoType(d)
         return d
     })
+
     surgery.sort((a, b) => a.year - b.year);
     analysis.sort((a, b) => a.year - b.year)
     // console.table(surgery[0])
     // console.log(surgery[0])
-    console.table(analysis[0])
-    console.log(analysis[0])
+    // console.table(analysis[0])
+    // console.log(analysis[0])
 
     const sumSry = d3.group(surgery, d => d.surgery);
     // console.log(sumSry)
 
+    const millions = analysis.filter(d => d.unit === "million");
+    const billions = analysis.filter(d => d.unit === "billion")
+    const sumMil = d3.group(millions, d => d.party);
+    const sumBil = d3.group(billions, d => d.party);
+    console.log(sumMil)
+    console.log(sumBil)
     //--------------------Create Accessors
 
     //surgeryType
@@ -84,17 +94,22 @@ async function drawAnalysis() {
     // console.log(rateAccessor(surgery[0]))
     const countAccessor = d => d.count;
 
+    //category
+    const categoryAccessor = d => d.category;
+    const valueAccessor = d => d.value;
+
 
     //set up Arrays manually so I can control the order;
-    const surgeryTypes = ["Breast augmentation", "Buttock augmentation", "Cheek implant", "Chin augmentation", "Facelift", "Lip augmentation", "Liposuction", "Nose reshaping", "Tummy tuck"]
-
-
+    const surgeryTypes = ["Breast augmentation", "Buttock surgeries", "Cheek implant", "Chin augmentation", "Facelift", "Lip augmentation", "Liposuction", "Nose reshaping", "Tummy tuck"]
+    const unit = ["million", "billion"]
+    const categories = ["Instagram Followers", "Networth", "Spending", "Ad Revenue"]
+    const parties = ["Kylie IG", "Kim IG", "Kylie Networth", "Kim Networth"]
     //*************************4. Create Scale  
 
 
     const timeScale = d3.scaleTime()
         .domain(d3.extent(surgery, yearAccessor))
-        .range([100, 400])
+        .range([100, 280])
         .nice()
 
     const rateScale = d3.scaleLinear()
@@ -107,6 +122,20 @@ async function drawAnalysis() {
         .range(['#8b0000', '#8b0000', '#808080', '#808080', '#808080', '#808080', '#808080', '#808080', '#808080'])
 
 
+    const milScale = d3.scaleLinear()
+        .domain(d3.extent(millions, valueAccessor))
+        .range([dimensionsLine.boundedHeight / 4 - 28, 50])
+        .nice()
+
+    const bilScale = d3.scaleLinear()
+        .domain(d3.extent(billions, valueAccessor))
+        .range([dimensionsLine.boundedHeight / 4 - 28, 50])
+        .nice()
+
+    const partyColorScale = d3.scaleOrdinal()
+        .domain(parties)
+        .range(['#11ae7a', '#808080', '#cbd438', '#8b0000'])
+
     //****************************5. Draw Peripherals
 
     //----------------------Draw Axis Generators
@@ -117,6 +146,12 @@ async function drawAnalysis() {
     const rateAxisGenerator = d3.axisLeft()
         .scale(rateScale)
 
+    const milAxisGenerator = d3.axisLeft()
+        .scale(milScale)
+
+    const bilAxisGenerator = d3.axisRight()
+        .scale(bilScale)
+
     //----------------------Draw Axes
 
     const timeAxis = boundsLine.append("g")
@@ -126,7 +161,22 @@ async function drawAnalysis() {
 
     const rateAxis = boundsLine.append("g")
         .call(rateAxisGenerator)
-        .style("transform", `translateX(${dimensionsLine.margin.left}px)`)
+        .style("transform", `translateX(100px)`)
+        .attr("class", "axisRed")
+
+    const timeDupeAxis = boundsLine.append("g")
+        .call(timeAxisGenerator)
+        .style("transform", `translate(220px,${dimensionsLine.boundedHeight / 4 - 28}px)`)
+        .attr("class", "axisRed")
+
+    const milAxis = boundsLine.append("g")
+        .call(milAxisGenerator)
+        .style("transform", `translateX(320px)`)
+        .attr("class", "axisRed")
+
+    const bilAxis = boundsLine.append("g")
+        .call(bilAxisGenerator)
+        .style("transform", `translateX(${dimensionsLine.boundedWidth - 100}px)`)
         .attr("class", "axisRed")
 
     //***************************6. Draw Chart Generators
@@ -137,6 +187,15 @@ async function drawAnalysis() {
         .curve(d3.curveCardinal)
     // .curve(d3.curveCatmullRom.alpha(0.9))
 
+    const lineMilGenerator = d3.line()
+        .x(d => timeScale(yearAccessor(d)))
+        .y(d => milScale(valueAccessor(d)))
+        .curve(d3.curveCardinal)
+
+    const lineBilGenerator = d3.line()
+        .x(d => timeScale(yearAccessor(d)))
+        .y(d => bilScale(valueAccessor(d)))
+        .curve(d3.curveCardinal)
 
     const lineChart = boundsLine.selectAll(".path")
         .data(sumSry)
@@ -145,6 +204,18 @@ async function drawAnalysis() {
         .transition()
         .attr("d", d => lineGenerator(d[1]))
         .attr("stroke", d => lineColorScale(d[0]))
+        .attr("stroke-width", 1)
+        .attr("fill", "none")
+        .attr("opacity", 1)
+
+
+    const lineMilChart = boundsLine.selectAll(".path")
+        .data(sumMil)
+        // .attr("class", "path")
+        .join("path")
+        .transition()
+        .attr("d", d => lineMilGenerator(d[1]))
+        .attr("stroke", d => partyColorScale(d[0]))
         .attr("stroke-width", 1)
         .attr("fill", "none")
         .attr("opacity", 1)
